83f2fd3227b35d8764fb057ab6a3cd5ced60b007
Murali Pachiappan <murali.pachiappan@bridgesgi.com>
2018-04-03 18:03:54 +0530

    Jar for WAS

---
+++ indgcustom/src/com/indigo/masterupload/itemfeedupload/IndgItemFeedUpload.java
@@ -0,0 +1,15 @@
+package com.indigo.masterupload.itemfeedupload;
+
+import com.bridge.sterling.framework.api.AbstractCustomApi;
+import com.yantra.yfc.dom.YFCDocument;
+import com.yantra.yfs.japi.YFSException;
+
+public class IndgItemFeedUpload extends AbstractCustomApi {
+
+  @Override
+  public YFCDocument invoke(YFCDocument inXml) throws YFSException {
+    
+    return null;
+  }
+
+}

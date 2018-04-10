0cc8bda6e3a9f9a05b88b81d13f897a537dd2938
kajol.singh <kajol.singh@bridgesgi.com>
2018-04-10 15:25:30 +0530

    Changes to Indg_StoreUpdate

--- indgcustom/src/com/indigo/masterupload/storeupdate/IndgStoreUpdates.java
+++ indgcustom/src/com/indigo/masterupload/storeupdate/IndgStoreUpdates.java
@@ -11,79 +11,126 @@ import com.yantra.yfc.core.YFCIterable;
 import com.yantra.yfc.dom.YFCDocument;
 import com.yantra.yfc.dom.YFCElement;
 
+/**
+ * 
+ * @author BGS168
+ * 
+ * Custom API to manage Stores to create and remove organizations
+ * based on Input document and the shipNodes that already exist.
+ * If organization preset in Input and but not in the system 
+ * manageOrganizationHierarchy will be called to create. If the 
+ * organization does not exist in InputDoc and but exist in System 
+ * then shipNode will be removed.If organization exist in both
+ * system and input doc publish to queue for manageOrganizationHierarchy.
+ *
+ */
+
 public class IndgStoreUpdates extends AbstractCustomApi {
 	
-	private String organizationCode = "";
-	private String shipNode = "";
 	private static final String EMPTY_STRING = "";
 	private static final String ORGANIZATION_SERVER= "OrganizationServer";
+	
+	/**	 
+	 * This method is the invoke point of the service.
+	 *  This calls manageDeltaLoadForDeletion method to
+	 *  get the list of shipNodes that need to be removed.
+	 * 
+	 */	
 
 	@Override
 	public YFCDocument invoke(YFCDocument inXml) {
-		   setOrganizationCode(inXml);
-		   YFCDocument shipNodeListApiOp = getShipNodeList(shipNode);
-		   Collection<String> uncommonShipNodeList = IndgManageDeltaLoadUtil.manageDeltaLoadForDeletion(inXml, shipNodeListApiOp, 
-				   XMLLiterals.ORGANIZATION_CODE,XMLLiterals.ORGANIZATION,XMLLiterals.SHIP_NODE_CODE,XMLLiterals.SHIPNODE);
-		   addInputDocToManageOrganizationHierarchy(inXml.getDocumentElement());
-		   manageExtraNodesInApiDoc(uncommonShipNodeList,shipNodeListApiOp);
-		   return inXml;
+	   YFCDocument shipNodeListApiOp = getShipNodeList();
+	   Collection<String> uncommonShipNodeList = IndgManageDeltaLoadUtil.manageDeltaLoadForDeletion(inXml, shipNodeListApiOp, 
+			   XMLLiterals.ORGANIZATION_CODE,XMLLiterals.ORGANIZATION,XMLLiterals.SHIP_NODE_CODE,XMLLiterals.SHIPNODE);
+	   addInputDocToManageOrganizationHierarchy(inXml.getDocumentElement());
+	   manageExtraNodesInApiDoc(uncommonShipNodeList,shipNodeListApiOp);
+	   return inXml;
 	}
 	
-	//get organizationCode from Input Doc
-	private void setOrganizationCode(YFCDocument inXml){
-		organizationCode = XPathUtil.getXpathAttribute(inXml, "/StoreList/Organization/@OrganizationCode");
+	/**
+	 * This method forms the Input XML for getShipNodeListAPI
+	 * 
+	 * @return
+	 */
+	
+	public YFCDocument formInputXmlForGetStoreList() {
+	    YFCDocument getStoreListDoc = YFCDocument.createDocument(XMLLiterals.SHIPNODE);
+	    getStoreListDoc.getDocumentElement().setAttribute(XMLLiterals.OWNERKEY, XMLLiterals.INDIGO_CA);
+	    getStoreListDoc.getDocumentElement().setAttribute(XMLLiterals.SHIPNODE, EMPTY_STRING);
+	    return getStoreListDoc;
+	 }
+	 
+	 /**
+	  * This method forms template for the getShipNodeList
+	  * 
+	  * @return
+	  */
+		
+	public YFCDocument formTemplateXmlForgetShipNodeList() {
+	    YFCDocument getShipNodeListTemp = YFCDocument.createDocument(XMLLiterals.SHIPNODE_LIST);
+	    YFCElement shipNodeEle = getShipNodeListTemp.getDocumentElement().createChild(XMLLiterals.SHIPNODE);
+	    shipNodeEle.setAttribute(XMLLiterals.SHIP_NODE_CODE, EMPTY_STRING);
+	    YFCElement organizationEle = shipNodeEle.createChild(XMLLiterals.ORGANIZATION);
+	    organizationEle.setAttribute(XMLLiterals.CAPACITYORGCODE, XMLLiterals.INDIGO_CA);
+	    organizationEle.setAttribute(XMLLiterals.PMENTERPRISEKEY, XMLLiterals.INDIGO_CA);
+	    return getShipNodeListTemp;
+	 }
+	
+	/**
+	 * This method calls getShipNodeList API
+	 * 
+	 * @return
+	 */
+		
+	public YFCDocument getShipNodeList(){
+	    return  invokeYantraApi(XMLLiterals.GET_SHIP_NODE_LIST, formInputXmlForGetStoreList(),formTemplateXmlForgetShipNodeList());
+	 }
+		
+	/**
+	 * This forms Input Document for manageOrganizationHierarchy 
+	 * and calls service to add the Document into Queue
+	 * 
+	 * @param inputEle
+	 */
+	
+	private void addInputDocToManageOrganizationHierarchy(YFCElement inputEle){
+	     YFCIterable<YFCElement> yfsItator = inputEle.getChildren(XMLLiterals.ORGANIZATION);
+	     for(YFCElement organizationEle: yfsItator) {
+	       YFCDocument inputDocForService = YFCDocument.createDocument(XMLLiterals.STORE_LIST);
+	       inputDocForService.getDocumentElement().importNode(organizationEle);
+	       callOrganizationUpdateQ(inputDocForService);
+	     }
 	   }
 	
-	//calls the getShipNodeList API and return API Input Doc
-	 public YFCDocument getShipNodeList(String shipno){
-	     return  invokeYantraApi(XMLLiterals.GET_SHIP_NODE_LIST, formInputXmlForGetStoreList(shipno),formTemplateXmlForgetShipNodeList());
+	/**
+	 * This method calls custom server to drop messages into queue
+	 * 
+	 * @param inputDocForService
+	 */
+	
+	private void callOrganizationUpdateQ(YFCDocument inputDocForService) {
+	     invokeYantraService(ORGANIZATION_SERVER, inputDocForService);
 	   }
-	 
-	 
-	 public YFCDocument formInputXmlForGetStoreList(String shipno) {
-		    YFCDocument getStoreListDoc = YFCDocument.createDocument(XMLLiterals.SHIPNODE);
-		    getStoreListDoc.getDocumentElement().setAttribute(XMLLiterals.OWNERKEY, XMLLiterals.INDIGO_CA);
-		    getStoreListDoc.getDocumentElement().setAttribute(XMLLiterals.SHIPNODE, shipno);
-		    getStoreListDoc.getDocumentElement().setAttribute(XMLLiterals.SHIPNODEKEY, shipno);
-		    return getStoreListDoc;
-		  }
 		
-		public YFCDocument formTemplateXmlForgetShipNodeList() {
-		    YFCDocument getShipNodeListTemp = YFCDocument.createDocument(XMLLiterals.SHIPNODE_LIST);
-		    YFCElement shipNodeEle = getShipNodeListTemp.getDocumentElement().createChild(XMLLiterals.SHIPNODE);
-		    shipNodeEle.setAttribute(XMLLiterals.SHIP_NODE_CODE, EMPTY_STRING);
-		    YFCElement organizationEle = shipNodeEle.createChild(XMLLiterals.ORGANIZATION);
-		    organizationEle.setAttribute(XMLLiterals.CAPACITYORGCODE, XMLLiterals.INDIGO_CA);
-		    organizationEle.setAttribute(XMLLiterals.PMENTERPRISEKEY, XMLLiterals.INDIGO_CA);
-		    return getShipNodeListTemp;
-		  }
-		
-		//form input doc for ManageOrganizationHierarchy and Calls service to drop into queue
-		private void addInputDocToManageOrganizationHierarchy(YFCElement inputEle){
-		     YFCIterable<YFCElement> yfsItator = inputEle.getChildren(XMLLiterals.ORGANIZATION);
-		     for(YFCElement organizationEle: yfsItator) {
-		       YFCDocument inputDocForService = YFCDocument.createDocument(XMLLiterals.STORE_LIST);
-		       inputDocForService.getDocumentElement().importNode(organizationEle);
-		       callOrganizationUpdate_Q(inputDocForService);
-		     }
-		   }
-		
-		//Call custom Server
-		private void callOrganizationUpdate_Q(YFCDocument inputDocForService) {
-		     invokeYantraService(ORGANIZATION_SERVER, inputDocForService);
-		   }
-		
-		//Iterates and delete the extra node in API input doc
-		private void manageExtraNodesInApiDoc(Collection<String> organizationList, YFCDocument shipNodeListAPIDoc) {
-		    for(String organizationId:organizationList) {
-		      YFCElement inEle = XPathUtil.getXPathElement(shipNodeListAPIDoc, "/StoreList/Organization[@OrganizationKey = "+organizationId+"]");
-		      if(!XmlUtils.isVoid(inEle)) {
-		        inEle.setAttribute(XMLLiterals.ACTION, XMLLiterals.DELETE);
-		        YFCDocument inputDocForService = YFCDocument.createDocument(XMLLiterals.STORE_LIST);
-		        inputDocForService.getDocumentElement().importNode(inEle);
-		        callOrganizationUpdate_Q(inputDocForService);
-		      }
-		    }
-		}
+	/**
+	 * This method iterates the Collection List and changes the Default
+	 * Action of Create or Modify to Delete so to remove the extra
+	 * shipNodes present in the system.
+	 * 
+	 * @param organizationList
+	 * @param shipNodeListAPIDoc
+	 */
+	
+	private void manageExtraNodesInApiDoc(Collection<String> organizationList, YFCDocument shipNodeListAPIDoc) {
+	    for(String organizationId:organizationList) {
+	      YFCElement inEle = XPathUtil.getXPathElement(shipNodeListAPIDoc, "/StoreList/Organization[@OrganizationKey = "+organizationId+"]");
+	      if(!XmlUtils.isVoid(inEle)) {
+	        inEle.setAttribute(XMLLiterals.ACTION, XMLLiterals.DELETE);
+	        YFCDocument inputDocForService = YFCDocument.createDocument(XMLLiterals.STORE_LIST);
+	        inputDocForService.getDocumentElement().importNode(inEle);
+	        callOrganizationUpdateQ(inputDocForService);
+	      }
+	    }
+	}
 
 }

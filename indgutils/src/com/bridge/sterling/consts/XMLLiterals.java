069ed416426afb056dac675973fff76e1fac8ee6
kajol.singh <kajol.singh@bridgesgi.com>
2018-04-13 19:46:44 +0530

    new XMLLiterals file

--- indgutils/src/com/bridge/sterling/consts/XMLLiterals.java
+++ indgutils/src/com/bridge/sterling/consts/XMLLiterals.java
@@ -1,6 +1,5 @@
 package com.bridge.sterling.consts;
 
-
 public class XMLLiterals {
 public static final String CATEGORY = "Category";
 public static final String CATEGORY_ID = "CategoryID";
@@ -26,9 +25,32 @@ public static final String CAPACITYORGCODE = "CapacityOrganizationCode";
 public static final String PMENTERPRISEKEY = "PrimaryEnterpriseKey";
 public static final String SHIP_NODE_LIST = "ShipNodeList";
 public static final String INDIGO_CA = "Indigo_CA";
+public static final String STORE_LIST = "StoreList";
 public static final String SHIPNODEKEY = "ShipnodeKey";
 public static final String ACTION = "Action";
 public static final String DELETE = "Delete";
+public static final String ITEM = "Item";
+public static final String ITEM_LIST = "ItemList";
+public static final String ITEM_ID = "ItemID";
+public static final String SYNC_TS = "SyncTS";
+public static final String GET_ITEM_LIST_API = "getItemList";
+public static final String CLASSIFICATION_CODES = "ClassificationCodes";
+public static final String PRODUCT_LINE = "ProductLine";
+public static final String MODIFY_CATEGORY_ITEM = "modifyCategoryItem";
+public static final String MODIFY_CATEGORY_ITEMS = "ModifyCategoryItems";
+public static final String CALLING_ORGANIZATION_CODE = "CallingOrganizationCode";
+public static final String CATEGORY_ITEM_LIST = "CategoryItemList";
+public static final String CATEGORY_ITEM = "CategoryItem";
+public static final String UNIT_OF_MEASURE = "UnitOfMeasure";
+public static final String MANAGE_ITEM = "manageItem";
+public static final String COMMODITY_CODE = "CommodityCode";
+public static final String SHORT_DESCRIPTION = "ShortDescription" ;
+public static final String DEPARTMENT_LIST = "DepartmentList";
+public static final String DEPARTMENT = "Department";
+public static final String DEPARTMENT_KEY = "DepartmentKey";
+public static final String DEPARTMENT_NAME = "DepartmentName";
+public static final String DEPARTMENT_CODE = "DepartmentCode";
+public static final String CATEGORY_FILTER = "CategoryFilter";
 public static final String MODIFY_USER_HIERARCHY = "modifyUserHierarchy";
 public static final String CREATE_USER_HIERARCHY = "createUserHierarchy";
 public static final String GET_USER_LIST = "getUserList";
@@ -43,5 +65,4 @@ public static final String ACTIVATE_FLAG = "Activateflag";
 public static final String ACTIVATEFLAG = "ActivateFlag";
 public static final String NODE = "Node";
 public static final String MANAGE_ORGANIZATION_HIERARCHY = "manageOrganizationHierarchy";
-public static final String ITEM_ID = "ItemID";
 }

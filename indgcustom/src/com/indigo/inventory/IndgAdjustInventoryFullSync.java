39deeb3075981618564ca1d8d1cba5e482cfe34d
Murali Pachiappan <murali.pachiappan@bridgesgi.com>
2018-05-09 15:37:02 +0530

    Inventory Sync Full and Delta

---
+++ indgcustom/src/com/indigo/inventory/IndgAdjustInventoryFullSync.java
@@ -0,0 +1,121 @@
+package com.indigo.inventory;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.bridge.sterling.consts.ExceptionLiterals;
+import com.bridge.sterling.consts.XMLLiterals;
+import com.bridge.sterling.framework.api.AbstractCustomApi;
+import com.bridge.sterling.utils.ExceptionUtil;
+import com.yantra.yfc.core.YFCIterable;
+import com.yantra.yfc.dom.YFCDocument;
+import com.yantra.yfc.dom.YFCElement;
+
+/**
+ * 
+ * Custom API to updates the full sync information to
+ * Custom Inventory Sync table and adjust the inventory according
+ * the input from the queue for Full Sync. 
+ * 
+ * @author BSG109
+ *
+ */
+public class IndgAdjustInventoryFullSync extends AbstractCustomApi{
+
+  @Override
+  public YFCDocument invoke(YFCDocument inXml) {
+    invokeYantraApi(XMLLiterals.ADJUST_INVENTORY_API, 
+        getInputForAdjustInventoryMisMatch(inXml));
+    return inXml;
+  }
+  
+  /**
+   * This method forms input XML for the inventoryAdjustment from the
+   * mismatch output and updates Sync Table
+   * 
+   * @param inXml
+   * @return
+   */
+  private YFCDocument getInputForAdjustInventoryMisMatch(YFCDocument inXml){
+    
+    YFCDocument adjustInventoryDoc = YFCDocument.createDocument(XMLLiterals.ITEMS);
+    YFCElement inputEle = inXml.getDocumentElement();
+    YFCIterable<YFCElement> yfsItator = inputEle.getChildren(XMLLiterals.ITEM);
+    String shipNode = inputEle.getChildElement(XMLLiterals.ITEM).getAttribute(XMLLiterals.SHIPNODE);
+    for(YFCElement itemEle : yfsItator) {
+      if(itemEle.hasAttribute(XMLLiterals.QUANTITY)) {
+        itemEle.setAttribute(XMLLiterals.ADJUSTMENT_TYPE, XMLLiterals.ADJUSTMENT);
+        adjustInventoryDoc.getDocumentElement().importNode(itemEle);
+      }
+      updateInventorySyncTable(itemEle);
+    }
+    deleteFullSyncStatusRecord(inputEle.getAttribute(XMLLiterals.INDG_FULL_SYNC_STATUS_KEY),shipNode);
+    return adjustInventoryDoc;
+  }
+  
+  /**
+   * 
+   * 
+   * 
+   * @param itemEle
+   */
+  public void updateInventorySyncTable(YFCElement itemEle) {
+    String itemID = itemEle.getAttribute(XMLLiterals.ITEM_ID);
+    String transactionDate = itemEle.getAttribute(XMLLiterals.TRANSACTION_DATE);
+    String generationDate = itemEle.getAttribute(XMLLiterals.GENERATION_DATE);
+    String shipNode = itemEle.getAttribute(XMLLiterals.SHIPNODE);
+    String selectQry = "SELECT * FROM indg_inv_sync_ctrl WHERE "
+        + "ITEM_ID = '"+itemID+"' AND SHIP_NODE='"+shipNode+"'";
+    Statement stmt = null;
+    ResultSet rset = null;
+    Connection conn = getDBConnection();
+    try{
+      stmt = conn.createStatement();
+        rset = stmt.executeQuery(selectQry);
+        if(!rset.isBeforeFirst()) {
+          manageInvSyncTable(itemID,shipNode,transactionDate,generationDate,XMLLiterals.CREATE_FULL_SYNC);
+        } else {
+          manageInvSyncTable(itemID,shipNode,transactionDate,generationDate,XMLLiterals.UPDATE_FULL_SYNC);
+        }
+        rset.close();
+        stmt.close();
+    }  catch(Exception exp) {
+      throw ExceptionUtil.getYFSException(ExceptionLiterals.ERRORCODE_SQL_EXP, exp);
+    }
+  }
+  
+  /**
+   * 
+   * 
+   * @param itemID
+   * @param ShipNode
+   * @param syncTS
+   */
+  private void manageInvSyncTable(String itemID, String shipNode,String transactionDate,String generationDate,String serviceName){
+    YFCDocument inXml = YFCDocument.createDocument(XMLLiterals.INDG_INV_FULL_SYNC);
+    YFCElement inputEle = inXml.getDocumentElement();
+    inputEle.setAttribute(XMLLiterals.ITEM_ID, itemID);
+    inputEle.setAttribute(XMLLiterals.TRANSACTION_DATE, transactionDate);
+    inputEle.setAttribute(XMLLiterals.GENERATION_DATE, generationDate);
+    inputEle.setAttribute(XMLLiterals.SHIPNODE, shipNode);
+    inputEle.setAttribute(XMLLiterals.SUPPLY_TYPE, XMLLiterals.ON_HAND);
+    invokeYantraService(serviceName, inXml);
+  }
+  
+  
+  /**
+   * This method delete record from Full Sync status table
+   * 
+   * 
+   * @param invSyncEOFKey
+   * @param shipNode
+   */
+  private void deleteFullSyncStatusRecord(String fullSyncStatusKey,String shipNode){
+    YFCDocument inXml = YFCDocument.createDocument(XMLLiterals.INDG_FULL_SYNC_STATUS);
+    YFCElement inEle = inXml.getDocumentElement();
+    inEle.setAttribute(XMLLiterals.INDG_FULL_SYNC_STATUS_KEY,fullSyncStatusKey);
+    inEle.setAttribute(XMLLiterals.SHIPNODE,shipNode);
+    invokeYantraService(XMLLiterals.INDG_FULL_SYNC_STATUS_DELETE, inXml);
+  }
+}
---
+++ indgcustom/src/com/indigo/inventory/IndgManageInventoryMismatch.java
@@ -0,0 +1,165 @@
+package com.indigo.inventory;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+import com.bridge.sterling.consts.ExceptionLiterals;
+import com.bridge.sterling.consts.XMLLiterals;
+import com.bridge.sterling.framework.api.AbstractCustomApi;
+import com.bridge.sterling.utils.ExceptionUtil;
+import com.bridge.sterling.utils.XPathUtil;
+import com.sterlingcommerce.tools.datavalidator.XmlUtils;
+import com.yantra.yfc.core.YFCIterable;
+import com.yantra.yfc.dom.YFCDocument;
+import com.yantra.yfc.dom.YFCElement;
+import com.yantra.yfs.japi.YFSException;
+
+/**
+ * 
+ * Custom API to get the mismatch and process the input for
+ * adjust location inventory.
+ * 
+ * @author BSG109
+ *
+ */
+public class IndgManageInventoryMismatch extends AbstractCustomApi {
+
+  private static final String FULL_SYNC_QUEUE_FLOW = "FULL_SYNC_QUEUE_FLOW";
+  private static final String TRUNCATE_LOG_QUERY = "TRUNCATE TABLE INDG_INV_ADJUSTMENT_LOG";
+  private static final int INITAL_ITRATOR_COUNT = 1;
+  private static final int MAX_ITEM_ELEMENT_COUNT = 3;
+  private static final String EMPTY_STRING = "";
+  private static final String INDG = "INDG";
+  
+  /**
+   * 
+   * This is the stating point for the custom API
+   * @throws YFSException 
+   * 
+   */
+  @Override
+  public YFCDocument invoke(YFCDocument inXml) {
+    truncateInventoryLogTable();
+    YFCDocument mismatchDoc = getInventoryMisMatch(inXml);
+    String shipNode = inXml.getDocumentElement().getChildElement(XMLLiterals.SHIPNODE)
+        .getAttribute(XMLLiterals.SHIPNODE);
+    formInputForAdjust(inXml,mismatchDoc);
+    formInputForFullAdjustInventory(mismatchDoc,shipNode);
+    return inXml;
+  }
+  
+  /**
+   * This method calls getInventoryMismatch API to get the
+   * mismatch between the Input FEED and OMS inventory
+   * 
+   * @param inXml
+   */
+  private YFCDocument getInventoryMisMatch(YFCDocument inXml){
+    return invokeYantraApi("getInventoryMismatch", inXml);
+  }
+  
+  /**
+   * 
+   * This method splits mismatch output document to 100 per document
+   * and calls InventoryFullSyncQ for adjust inventory.
+   * 
+   * @param inXml
+   * @param mismatchDoc
+   */
+  private void formInputForFullAdjustInventory(YFCDocument mismatchDoc,String shipNode) {
+    YFCDocument adjustInventoryDoc = YFCDocument.createDocument(XMLLiterals.ITEMS);
+    String manageInventoryService = getProperty(FULL_SYNC_QUEUE_FLOW);
+    YFCIterable<YFCElement> yfcItrator = mismatchDoc.getDocumentElement()
+        .getChildren(XMLLiterals.ITEM);
+    int itratorCount = INITAL_ITRATOR_COUNT;
+    Date dateKey = new Date();
+    String invSyncStatusKey =INDG+dateKey.getTime();
+    adjustInventoryDoc.getDocumentElement().setAttribute(XMLLiterals.INDG_FULL_SYNC_STATUS_KEY,
+        invSyncStatusKey);
+    addRecordToManageStatusTable(invSyncStatusKey,shipNode);
+    for(YFCElement itemEle : yfcItrator) {
+      if(itratorCount == MAX_ITEM_ELEMENT_COUNT) {
+        dateKey = new Date();
+        invSyncStatusKey = INDG+dateKey.getTime();
+        addRecordToManageStatusTable(invSyncStatusKey,shipNode);
+        invokeYantraService(FULL_SYNC_QUEUE_FLOW, adjustInventoryDoc);
+        adjustInventoryDoc = YFCDocument.createDocument(XMLLiterals.ITEMS);
+        adjustInventoryDoc.getDocumentElement().setAttribute(XMLLiterals.INDG_FULL_SYNC_STATUS_KEY,
+            invSyncStatusKey);
+        itratorCount = INITAL_ITRATOR_COUNT;
+      }
+      itemEle.setAttribute(XMLLiterals.ADJUSTMENT_TYPE, XMLLiterals.ADJUSTMENT);
+      adjustInventoryDoc.getDocumentElement().importNode(itemEle);
+      itratorCount++;
+    }
+    invokeYantraService(manageInventoryService, adjustInventoryDoc);
+  }
+  
+  /**
+   * This method truncate the data from the 
+   * INDG_INVENTORY_LOG table to start Full sync and mismatch process
+   * @throws SQLException 
+   * 
+   */
+  private void truncateInventoryLogTable() {
+      Statement stmt = null;
+      ResultSet rset = null;
+      try{
+          Connection conn = getDBConnection();
+          stmt = conn.createStatement();
+          rset = stmt.executeQuery(TRUNCATE_LOG_QUERY);
+          rset.close();
+          stmt.close();
+      }  catch(Exception exp) {
+        throw ExceptionUtil.getYFSException(ExceptionLiterals.ERRORCODE_SQL_EXP, exp);
+      }
+  }
+  
+  /**
+   * This method forms input for Inventory Adjustment 
+   * 
+   * @param inXml
+   * @param misMatchDoc
+   */
+  private void formInputForAdjust(YFCDocument inXml, YFCDocument misMatchDoc){
+    YFCElement shipNodeEle = inXml.getDocumentElement().getChildElement(XMLLiterals.SHIPNODE);
+    String shipNode = shipNodeEle.getAttribute(XMLLiterals.SHIPNODE);
+    YFCIterable<YFCElement> yfsItrator = shipNodeEle.getChildren(XMLLiterals.ITEM);
+    for(YFCElement itemEle:yfsItrator) {
+      String itemID = itemEle.getAttribute(XMLLiterals.ITEM_ID);
+      String generationDate = itemEle.getAttribute(XMLLiterals.GENERATION_DATE,EMPTY_STRING);
+      String transactionDate = itemEle.getAttribute(XMLLiterals.TRANSACTION_DATE,EMPTY_STRING);
+      YFCElement misMatchItemEle = XPathUtil.getXPathElement(misMatchDoc, "/Items/Item[@ItemID=\""+itemID+"\"]");
+      if(XmlUtils.isVoid(misMatchItemEle)) {
+        YFCElement tempEle = misMatchDoc.getDocumentElement().createChild(XMLLiterals.ITEM);
+        tempEle.setAttribute(XMLLiterals.ITEM_ID,itemID);
+        tempEle.setAttribute(XMLLiterals.GENERATION_DATE,generationDate);
+        tempEle.setAttribute(XMLLiterals.TRANSACTION_DATE,generationDate);
+        tempEle.setAttribute(XMLLiterals.SHIPNODE, shipNode);
+      } else {
+        misMatchItemEle.setAttribute(XMLLiterals.GENERATION_DATE,generationDate);
+        misMatchItemEle.setAttribute(XMLLiterals.TRANSACTION_DATE,transactionDate);
+        misMatchItemEle.setAttribute(XMLLiterals.ADJUSTMENT_TYPE, XMLLiterals.ADJUSTMENT);
+        misMatchItemEle.setAttribute(XMLLiterals.SHIPNODE, shipNode);
+      }
+    }
+  }
+  
+  
+  /**
+   * This method inserts record into Status manage table to
+   * control End of File.
+   * 
+   * @param shipNode
+   */
+  private void addRecordToManageStatusTable(String invSyncStatusKey,String shipNode){
+    YFCDocument inXml = YFCDocument.createDocument(XMLLiterals.INDG_FULL_SYNC_STATUS);
+    YFCElement inEle = inXml.getDocumentElement();
+    inEle.setAttribute(XMLLiterals.INDG_FULL_SYNC_STATUS_KEY,invSyncStatusKey);
+    inEle.setAttribute(XMLLiterals.SHIPNODE, shipNode);
+    invokeYantraService(XMLLiterals.INDG_FULL_SYNC_STATUS_CREATE, inXml);
+  }
+}

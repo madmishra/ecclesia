0f235df4a948aa1b2637dcec221fa9a907b7f0e5
Murali Pachiappan <murali.pachiappan@bridgesgi.com>
2018-05-15 12:12:42 +0530

    Literals changes

--- indgutils/src/com/bridge/sterling/consts/XMLLiterals.java
+++ indgutils/src/com/bridge/sterling/consts/XMLLiterals.java
@@ -200,5 +200,9 @@ public static final String ORGANIZATION_LIST = "OrganizationList";
 public static final String GET_ORGANIZATION_LIST = "getOrganizationList";
 public static final String TRANSACTION_DATE = "TransactionDate";
 public static final String GENERATION_DATE = "GenerationDate";
+public static final String QUANTITY = "Quantity";
+public static final String ITEMS = "Items";
+public static final String SUPPLY_TYPE = "SupplyType";
+public static final String ON_HAND = "ONHAND";
 }
 

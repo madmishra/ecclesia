19e80ada8677449a794a8e114798c1f49f2c4bd0
Murali Pachiappan <murali.pachiappan@bridgesgi.com>
2018-05-16 17:57:53 +0530

    Inventory Sync Changes

--- indgcustom/src/com/indigo/inventory/IndgManageFullSyncStatus.java
+++ indgcustom/src/com/indigo/inventory/IndgManageFullSyncStatus.java
@@ -19,6 +19,7 @@ public class IndgManageFullSyncStatus extends AbstractCustomApi {
   private static final String INDG_FULL_SYNC_STATUS_LIST_FLOW= "IndgGetSyncStatusList";
   private static final String INITIAL_SLEEP_TIME = "InitialSleepTime";
   private static final String DELTA_SLEEP_TIME = "DeltaSleepTime";
+  private static final String SCRIPT_PATH = "SCRIPT_PATH";
 
 /**
  * This method is the invoke point of the class. This method
@@ -30,8 +31,6 @@ public class IndgManageFullSyncStatus extends AbstractCustomApi {
  */
   @Override
   public YFCDocument invoke(YFCDocument inXml) {
-    
-    
     int initialSleepTime = Integer.parseInt(getProperty(INITIAL_SLEEP_TIME));
     try {
       Thread.sleep(initialSleepTime);
@@ -63,7 +62,7 @@ public class IndgManageFullSyncStatus extends AbstractCustomApi {
    */
   private void sleepTillEOF() {
     int deltaSleepTime = Integer.parseInt(getProperty(DELTA_SLEEP_TIME));
-    String myShellScript = "./startServer.sh";
+    String myShellScript = getProperty(SCRIPT_PATH);
     try {
       Thread.sleep(deltaSleepTime);
       YFCDocument fullSyncStatusList = isFullSyncCompleted();

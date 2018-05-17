476e713c21996f4f9976a08c14ec084bb59aea2b
kajol.singh <kajol.singh@bridgesgi.com>
2018-05-17 17:17:41 +0530

    change9

--- indgcustom/src/com/indigo/om/outbound/api/CancelMissingLines.java
+++ indgcustom/src/com/indigo/om/outbound/api/CancelMissingLines.java
@@ -178,9 +178,9 @@ public class CancelMissingLines extends AbstractCustomApi{
 				getChildElement(XMLLiterals.ORDER).getAttribute(XMLLiterals.RELEASE_NO);
 		inputDocForChangeOrderAPI.getDocumentElement().setAttribute(XMLLiterals.MODIFYTS, modifyTs);
 		System.out.println("<<<<<<CHILDORDERNO>>>>>>>>>"+childOrderNo);
-		inputDocForChangeOrderAPI.getDocumentElement().getChildElement(XMLLiterals.ORDER_LINES).
-		getChildElement(XMLLiterals.ORDER_LINE).
-		getChildElement(XMLLiterals.EXTN).setAttribute(XMLLiterals.EXTN_LEGACY_OMS_CHILD_ORDERNO, childOrderNo);
+		YFCElement extnEle = inputDocForChangeOrderAPI.getDocumentElement().getChildElement(XMLLiterals.ORDER_LINES).
+				getChildElement(XMLLiterals.ORDER_LINE).createChild(XMLLiterals.EXTN);
+		extnEle.setAttribute(XMLLiterals.EXTN_LEGACY_OMS_CHILD_ORDERNO, childOrderNo);
 		System.out.println("----------END OF LIFELINE"+inputDocForChangeOrderAPI);
 	
 	}

1847aa5d668f8b1e1b9dc0befa717758438f2fb0
Murali Pachiappan <murali.pachiappan@bridgesgi.com>
2018-05-17 13:09:21 +0530

    Inv Sync changes

--- Foundation/script/stopDeltaSyncServer.sh
+++ Foundation/script/stopDeltaSyncServer.sh
@@ -1,7 +1,7 @@
 PROCESS_COUNT=$(ps -ef | grep FullSync | grep -v "grep" | wc -l)
         if [ $PROCESS_COUNT -gt 0 ];
         then
-                cd /Indigo/OMS95/Foundation/bin
-                nohup ./stopIntegrationServer.sh -name DeltaSync /Indigo/OMS95/Foundation/logs/Delta_Sync_${date}.log &
+                cd /data/oms95/Foundation/bin
+                nohup ./stopIntegrationServer.sh -name IndgDeltaSync /data/oms95/Foundation/logs/Delta_Sync_${date}.log &
         exit	
         fi

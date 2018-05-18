eb4fbea1efe9f7e979736870d159b988760291fb
Murali Pachiappan <murali.pachiappan@bridgesgi.com>
2018-05-18 14:22:58 +0530

    Order Monitor changes

--- Foundation/script/stopDeltaSyncServer.sh
+++ Foundation/script/stopDeltaSyncServer.sh
@@ -2,6 +2,6 @@ PROCESS_COUNT=$(ps -ef | grep FullSync | grep -v "grep" | wc -l)
         if [ $PROCESS_COUNT -gt 0 ];
         then
                 cd /data/oms95/Foundation/bin
-                nohup ./stopIntegrationServer.sh -name IndgDeltaSync /data/oms95/Foundation/logs/Delta_Sync_${date}.log &
+                nohup ./stopIntegrationServer.sh -name IndgDeltaSync /data/oms95/Foundation/logs/Delta_Sync.log &
         exit	
         fi

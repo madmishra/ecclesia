c5fa7704115e09c3d572abb8317801d6227df720
kavya.s <kavya.s@bridgesgi.com>
2018-05-19 21:48:45 +0530

    order monitor changes

--- indgcustom/src/com/indigo/om/outbound/api/IndgOrderMonitorForOrderLine.java
+++ indgcustom/src/com/indigo/om/outbound/api/IndgOrderMonitorForOrderLine.java
@@ -23,10 +23,9 @@ import com.yantra.yfc.log.*;
  */
 
 public class IndgOrderMonitorForOrderLine extends AbstractCustomApi {
-	private static final String EMPTY_STRING="";
 	Map<String,String> orderLineMap = new HashMap<>();
 	private static final String ORDER_MONITOR="IndgOrderMonitor";
-	private static final String STATUS = "Created";	
+	private static final String STATUS = "1100";	
 	
 	/**
 	 * Logger for IndgOrderMonitorForOrderLine Instance 
@@ -39,83 +38,26 @@ public class IndgOrderMonitorForOrderLine extends AbstractCustomApi {
 	 */
 	
 	 public YFCDocument invoke(YFCDocument inXml)  {
-		 YFCElement inXmlEle=inXml.getDocumentElement();
-		 String orderNo=inXmlEle.getChildElement(XMLLiterals.ORDER).getAttribute(XMLLiterals.ORDER_NO);
-		 System.out.println(orderNo+"----------------------------orderNo-----------------------");
-		 String enterpriseCode=inXmlEle.getChildElement(XMLLiterals.ORDER).getAttribute(XMLLiterals.SELLER_ORGANIZATION_CODE);
-		 System.out.println("------------------enterpriseCode-------------"+enterpriseCode);
-		 String documentType=inXmlEle.getChildElement(XMLLiterals.ORDER).getAttribute(XMLLiterals.DOCUMENT_TYPE);
-		 System.out.println("-------------documentType-----"+documentType);
-		 YFCDocument orderDetailDoc = invokeYantraApi(XMLLiterals.GET_ORDER_DETAILS, 
-				 getOrderDetailsinput(orderNo,enterpriseCode,documentType), getOrderDetailsTemplate());
-		 getOrderDetailsGroupedByShipNode(orderDetailDoc);
-		return inXml;
-	 }
+			 YFCDocument outXML=getOrderDetailsGroupedByShipNode(inXml);
+			return outXML;
+		 }
 	 
-	 /**
-	  * This method is used to get all the attribute values from the input
-	  * 
-	  * @param orderNo
-	  * @param enterpriseCode
-	  * @param documentType
-	  * @return
-	  */
-	 
-	 public YFCDocument getOrderDetailsinput(String orderNo,String enterpriseCode,String documentType ) {
-		 YFCDocument getOrderDetailsinput=YFCDocument.createDocument(XMLLiterals.ORDER);
-		 YFCElement orderEle=getOrderDetailsinput.getDocumentElement();
-		 orderEle.setAttribute(XMLLiterals.ORDER_NO,orderNo);
-		 orderEle.setAttribute(XMLLiterals.DOCUMENT_TYPE,documentType);
-		 orderEle.setAttribute(XMLLiterals.ENTERPRISE_CODE,enterpriseCode);
-		 System.out.println("--------------getOrderDetailsinputDocument-------"+getOrderDetailsinput);
-		// LoggerUtil.verboseLog("=================getOrderDetailsinput========================", logger, getOrderDetailsinput);
-		 return getOrderDetailsinput;
-	 }
-	 
-	 /**
-	  * This method is used for creating the getOrderDetails template
-	  * 
-	  * @return
-	  */
-	 
-	 public YFCDocument getOrderDetailsTemplate() {
-		 YFCDocument getOrderDetailsTempDoc=YFCDocument.createDocument(XMLLiterals.ORDER);
-		 YFCElement orderEle=getOrderDetailsTempDoc.getDocumentElement();
-		 orderEle.setAttribute(XMLLiterals.ORDER_NO,EMPTY_STRING);
-		 YFCElement orderLineEle=orderEle.createChild(XMLLiterals.ORDER_LINES).createChild(XMLLiterals.ORDER_LINE);
-		 orderLineEle.setAttribute(XMLLiterals.STATUS, EMPTY_STRING);
-		 orderLineEle.setAttribute(XMLLiterals.PRIME_LINE_NO, EMPTY_STRING);
-		 orderLineEle.setAttribute(XMLLiterals.SHIPNODE, EMPTY_STRING);
-		 YFCElement extnEle=orderLineEle.createChild(XMLLiterals.EXTN);
-		 extnEle.setAttribute(XMLLiterals.EXTN_LEGACY_OMS_CHILD_ORDERNO, EMPTY_STRING);
-		 //LoggerUtil.verboseLog("=================getOrderDetailsTempDoc========================", logger, getOrderDetailsTempDoc);
-		System.out.println("-----getOrderDetailsTempDoc-----"+getOrderDetailsTempDoc);
-		 return getOrderDetailsTempDoc;
-		 
-	 }
-	 
-	 
-	 /**
-	   * 
-	   * This method iterate throw the OrderLines and add
-	   * them to the Map with list of OrderLine Elements. 
-	   * 
-	   */
-	  private void getOrderDetailsGroupedByShipNode(YFCDocument inputDocForOrderMonitor){
-	    YFCElement orderElement = inputDocForOrderMonitor.getDocumentElement().getChildElement(XMLLiterals.ORDER_LINES);
-	    YFCIterable<YFCElement> yfsItrator = orderElement.getChildren();
-	    for(YFCElement orderLine: yfsItrator) {
-	      String shipNode = orderLine.getAttribute(XMLLiterals.SHIPNODE);
-		  System.out.println("-----shipNode---"+shipNode);
-	     // LoggerUtil.verboseLog("=================ShipNode========================", logger, shipNode);
-	      if(XmlUtils.isVoid(orderLineMap.get(shipNode))) {
-	    	  orderLineMap.put(shipNode,shipNode);
-	    	  if(shipNode.equals(STATUS)) {
-	    		  invokeYantraService(getProperty(ORDER_MONITOR), inputDocForOrderMonitor);
-				  System.out.println("---------------ALERT-------");
-	    	  }
-	      }
-	    }
-	      
-	  }
-}
+	  private YFCDocument getOrderDetailsGroupedByShipNode(YFCDocument inXml){
+		    YFCElement orderElement = inXml.getDocumentElement().getChildElement(XMLLiterals.ORDER).getChildElement(XMLLiterals.ORDER_LINES);
+			System.out.println("-----orderElement---"+orderElement);
+		    YFCIterable<YFCElement> yfsItrator = orderElement.getChildren();
+		    for(YFCElement orderLine: yfsItrator) {
+		      String shipNode = orderLine.getChildElement(XMLLiterals.ORDER_STATUSES).getChildElement(XMLLiterals.ORDER_STATUS).getAttribute(XMLLiterals.STATUS);
+			  System.out.println("-----shipNode---"+shipNode);
+		     // LoggerUtil.verboseLog("=================ShipNode========================", logger, shipNode);
+		      if(XmlUtils.isVoid(orderLineMap.get(shipNode))) {
+		    	  //orderLineMap.put(shipNode,shipNode);
+		    	  if(shipNode.equals(STATUS)) {
+		    		  invokeYantraService(getProperty(ORDER_MONITOR), inXml);
+					  System.out.println("---------------ALERT----------");
+		    	  }
+		      }
+		    }
+		     return inXml;
+		  }
+	}

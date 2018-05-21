07a61a63b1ebf69d516ad985cc5e9bf8d05efbca
Murali Pachiappan <murali.pachiappan@bridgesgi.com>
2018-05-21 21:35:59 +0530

    Order Monitor changes

--- indgcustom/src/com/indigo/om/outbound/api/IndgOrderMonitorForOrderLine.java
+++ indgcustom/src/com/indigo/om/outbound/api/IndgOrderMonitorForOrderLine.java
@@ -1,93 +1,88 @@
 package com.indigo.om.outbound.api;
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
-import com.bridge.sterling.consts.ValueConstants;
 import com.bridge.sterling.consts.XMLLiterals;
 import com.bridge.sterling.framework.api.AbstractCustomApi;
-import com.bridge.sterling.utils.LoggerUtil;
-import com.bridge.sterling.utils.XMLUtil;
 import com.sterlingcommerce.tools.datavalidator.XmlUtils;
+import com.yantra.yfc.core.YFCIterable;
 import com.yantra.yfc.dom.YFCDocument;
 import com.yantra.yfc.dom.YFCElement;
-import com.yantra.yfc.dom.YFCNodeList;
-import com.yantra.yfc.log.*;
-
-
+import com.yantra.yfc.dom.YFCNode;
 
 /**
  * 
+ * This service will be called on Monitor Event to
+ * split Order by Ship Node
  * 
- * @author Nikita Shukla
+ * @author BSG109
  *
  */
-
-public class IndgOrderMonitorForOrderLine extends AbstractCustomApi {
-	Map<String,ArrayList<YFCElement>> orderLineMap = new HashMap<>();
-	private static final String ORDER_MONITOR="ORDER_MONITOR";
-	private static final String STATUS = "1100";	
-
-	/**
-	 * Logger for IndgOrderMonitorForOrderLine Instance 
-	 * 
-	 */
-	private static YFCLogCategory logger = YFCLogCategory.instance(IndgOrderMonitorForOrderLine.class);
-
-	/**
-	 * This is the main invoke method
-	 */
-
-	public YFCDocument invoke(YFCDocument inXml)  {
-		getOrderDetailsGroupedByShipNode(inXml);
-		return inXml;
-	}
-
-	/**
-	 * This method is creating a new Node List where all the OrderLines with created status is being published and creating the alert 
-	 * 
-	 */
-
-	private void getOrderDetailsGroupedByShipNode(YFCDocument inXml){
-
-		YFCNodeList<YFCElement> ordlineList = inXml.getElementsByTagName(XMLLiterals.ORDER_LINE);
-		for (YFCElement curOrdLineElement : ordlineList) {
-			String shipNode =curOrdLineElement.getAttribute(XMLLiterals.SHIPNODE,ValueConstants.EMPTY_STRING);
-			YFCNodeList<YFCElement> ordStatusEleList = curOrdLineElement.getChildElement(XMLLiterals.ORDER_STATUSES,true).getElementsByTagName(XMLLiterals.ORDER_STATUS);
-			System.out.println("Indigo OrdLineMap OrderStatusElementList==>ordStatusEleList ==============="+ordStatusEleList.toString());
-			for (YFCElement curOrdStatusEle : ordStatusEleList) {
-				String status = curOrdStatusEle.getAttribute(XMLLiterals.STATUS,ValueConstants.EMPTY_STRING);
-				if(STATUS.equals(status)) {
-					if(orderLineMap.containsKey(shipNode)) {
-						orderLineMap.get(shipNode).add(curOrdLineElement);
-					}else {
-						ArrayList<YFCElement> alertOrderLineList = new ArrayList<>();
-						alertOrderLineList.add(curOrdLineElement);
-						orderLineMap.put(shipNode, alertOrderLineList);
-					}
-				}
-				System.out.println("Indigo OrdLineMap Status Check==>status ==============="+status.toString());
-			}
-
-			System.out.println("Indigo OrdLineMap ShipNode==>OrdLine ==============="+orderLineMap.toString());
-
-			for (ArrayList<YFCElement> ordLineList : orderLineMap.values()) {
-				 YFCDocument inDocClone = inXml.getCopy();
-				  YFCElement ordLinesEle = inDocClone.getDocumentElement().getChildElement(XMLLiterals.ORDER,true).getChildElement(XMLLiterals.ORDER_LINES,true);
-				  ordLinesEle.removeChild(ordLinesEle);
-				  ordLinesEle = inDocClone.getDocumentElement().getChildElement(XMLLiterals.ORDER_LINES,true);			  
-				  System.out.println("Indigo ordLinesEle Status Check==>ordLinesEle ==============="+ordLinesEle.toString());
-				  for(int i=0; i<ordLineList.size() ; i++) {
-					 XMLUtil.importNode(ordLinesEle,ordLineList.get(i));
-				  }
-				  System.out.println("Indigo ordLineList ===================== "+ordLineList.toString());
-				  System.out.println(ordLineList.toString());
-				  invokeYantraService(getProperty(ORDER_MONITOR), inDocClone);		
-			}
-		}
-
-	}
-
+public class IndgOrderMonitorForOrderLine extends AbstractCustomApi{
+  Map<String,List<YFCElement>> orderLineMap = new HashMap<>();
+  private static final String ORDER_MONITOR="ORDER_MONITOR";
+  private static final String STATUS = "1100";
+  @Override
+  public YFCDocument invoke(YFCDocument inXml) {
+    String inputDocString = inXml.toString();
+    YFCDocument inputDocForMonitor = YFCDocument.getDocumentFor(inputDocString);
+    getOrderLinesGroupedByShipNode(inputDocForMonitor);
+    createChildOrderForMonitor(inputDocForMonitor);
+    return inXml;
+  }
+  
+  /**
+   * 
+   * This method iterate throw the OrderLines and add
+   * them to the Map with list of OrderLine Elements. 
+   * 
+   */
+  private void getOrderLinesGroupedByShipNode(YFCDocument inputDocForMonitor){
+    YFCElement orderLineListEle = inputDocForMonitor.getDocumentElement().getChildElement(XMLLiterals.ORDER).getChildElement(XMLLiterals.ORDER_LINES);
+    YFCIterable<YFCElement> yfsItrator = orderLineListEle.getChildren(XMLLiterals.ORDER_LINE);
+    for(YFCElement orderLine: yfsItrator) {
+      List<YFCElement> orderLineList;
+      String shipNode = orderLine.getAttribute(XMLLiterals.SHIPNODE);
+      String status = orderLine.getChildElement(XMLLiterals.ORDER_STATUSES).getChildElement(XMLLiterals.ORDER_STATUS)
+          .getAttribute(XMLLiterals.STATUS);
+      if (STATUS.equals(status)) {
+        if(XmlUtils.isVoid(orderLineMap.get(shipNode))) {
+          orderLineList = new ArrayList<>();
+          orderLineList.add(orderLine);
+          orderLineMap.put(shipNode,orderLineList);
+        } else {
+          orderLineList = orderLineMap.get(shipNode);
+          orderLineList.add(orderLine);
+          orderLineMap.put(shipNode,orderLineList);
+        }
+      }
+      YFCNode parent = orderLine.getParentNode();
+      parent.removeChild(orderLine);
+  }
+}
+  
+  /**
+   * 
+   * This method forms input for Monitor alert and invoke the 
+   * service to drop the message
+   * 
+   * @param inXml
+   */
+  private void createChildOrderForMonitor(YFCDocument inputDocForMonitor) {
+    for (Entry<String, List<YFCElement>> entry : orderLineMap.entrySet()) {
+      List<YFCElement> orderLineList = orderLineMap.get(entry.getKey());
+      String orderDocString = inputDocForMonitor.toString();
+      YFCDocument outputDocForMonitor = YFCDocument.getDocumentFor(orderDocString);
+      YFCElement orderLinesEle = outputDocForMonitor.getDocumentElement().getChildElement(XMLLiterals.ORDER)
+          .getChildElement(XMLLiterals.ORDER_LINES);
+      for(YFCElement lineEle : orderLineList) {
+        orderLinesEle.importNode(lineEle);
+      }
+      invokeYantraService(getProperty(ORDER_MONITOR), outputDocForMonitor);
+    }
+  }
 }

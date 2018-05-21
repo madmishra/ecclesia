de36e1a1af91b23509a538e6471b167325b25f4c
kavya.s <kavya.s@bridgesgi.com>
2018-05-21 15:03:30 +0530

    Order monitor changes

--- indgcustom/src/com/indigo/om/outbound/api/IndgOrderMonitorForOrderLine.java
+++ indgcustom/src/com/indigo/om/outbound/api/IndgOrderMonitorForOrderLine.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.bridge.sterling.consts.ValueConstants;
 import com.bridge.sterling.consts.XMLLiterals;
 import com.bridge.sterling.framework.api.AbstractCustomApi;
 import com.bridge.sterling.utils.LoggerUtil;
@@ -25,54 +26,65 @@ import com.yantra.yfc.log.*;
  */
 
 public class IndgOrderMonitorForOrderLine extends AbstractCustomApi {
-	Map<String,String> orderLineMap = new HashMap<>();
+	Map<String,ArrayList<YFCElement>> orderLineMap = new HashMap<>();
 	private static final String ORDER_MONITOR="IndgOrderMonitor";
 	private static final String STATUS = "1100";	
-	
+
 	/**
 	 * Logger for IndgOrderMonitorForOrderLine Instance 
 	 * 
 	 */
 	private static YFCLogCategory logger = YFCLogCategory.instance(IndgOrderMonitorForOrderLine.class);
-	
+
 	/**
 	 * This is the main invoke method
 	 */
-	
-	 public YFCDocument invoke(YFCDocument inXml)  {
-			 YFCDocument outXML=getOrderDetailsGroupedByShipNode(inXml);
-			return outXML;
-		 }
-	 
-	 /**
-	  * This method is creating a new Node List where all the OrderLines with created status is being published and creating the alert 
-	  * 
-	  */
-	 
-	  private YFCDocument getOrderDetailsGroupedByShipNode(YFCDocument inXml){
-		  
-		  YFCNodeList<YFCElement> ordlineList = inXml.getElementsByTagName(XMLLiterals.ORDER_LINE);
-		  ArrayList<YFCElement> alertOrderLineList = new ArrayList<>();
-		  for (YFCElement curOrdLineElement : ordlineList) {
-			  String shipNode = curOrdLineElement.getChildElement(XMLLiterals.ORDER_STATUSES,true).getChildElement(XMLLiterals.ORDER_STATUS,true).getAttribute(XMLLiterals.STATUS);
-			  System.out.println("-----shipNode---"+shipNode);
-		      LoggerUtil.verboseLog("=================ShipNode========================", logger, shipNode);
-		      if(XmlUtils.isVoid(orderLineMap.get(shipNode)) && shipNode.equals(STATUS)) {
-		    		  alertOrderLineList.add(curOrdLineElement);
-					  System.out.println("---------------ALERT----------");
-		      }
-		}
-		  
-		  if(!XmlUtils.isVoid(alertOrderLineList)) {
-			  YFCDocument inDocClone = inXml.getCopy();
-			  YFCElement ordLinesEle = inDocClone.getDocumentElement().getChildElement(XMLLiterals.ORDER_LINES,true);
-			  inDocClone.removeChild(ordLinesEle);
-			  ordLinesEle = inDocClone.getDocumentElement().getChildElement(XMLLiterals.ORDER_LINES,true);			  
-			  for(int i=0; i<alertOrderLineList.size() ; i++) {
-				 XMLUtil.importNode(ordLinesEle,alertOrderLineList.get(i));
-			  }
-			  invokeYantraService(getProperty(ORDER_MONITOR), inXml);			  
-		  }
-		     return inXml;
-		 }
+
+	public YFCDocument invoke(YFCDocument inXml)  {
+		getOrderDetailsGroupedByShipNode(inXml);
+		return inXml;
 	}
+
+	/**
+	 * This method is creating a new Node List where all the OrderLines with created status is being published and creating the alert 
+	 * 
+	 */
+
+	private void getOrderDetailsGroupedByShipNode(YFCDocument inXml){
+
+		YFCNodeList<YFCElement> ordlineList = inXml.getElementsByTagName(XMLLiterals.ORDER_LINE);
+
+		for (YFCElement curOrdLineElement : ordlineList) {
+			String shipNode =curOrdLineElement.getAttribute(XMLLiterals.SHIP_NODE,ValueConstants.EMPTY_STRING);
+			YFCNodeList<YFCElement> ordStatusEleList = curOrdLineElement.getChildElement(XMLLiterals.ORDER_STATUSES,true).getChildElement(XMLLiterals.ORDER_STATUS,true).getElementsByTagName(XMLLiterals.ORDER_STATUS);
+			for (YFCElement curOrdStatusEle : ordStatusEleList) {
+				String status = curOrdStatusEle.getAttribute(XMLLiterals.STATUS,ValueConstants.EMPTY_STRING);
+				if(STATUS.equals(status)) {
+					if(orderLineMap.containsKey(shipNode)) {
+						orderLineMap.get(shipNode).add(curOrdLineElement);
+					}else {
+						ArrayList<YFCElement> alertOrderLineList = new ArrayList<>();
+						alertOrderLineList.add(curOrdLineElement);
+						orderLineMap.put(shipNode, alertOrderLineList);
+					}
+				}
+			}
+
+			System.out.println(orderLineMap.toString());
+
+			for (ArrayList<YFCElement> ordLineList : orderLineMap.values()) {
+				 YFCDocument inDocClone = inXml.getCopy();
+				  YFCElement ordLinesEle = inDocClone.getDocumentElement().getChildElement(XMLLiterals.ORDER,true).getChildElement(XMLLiterals.ORDER_LINES,true);
+				  inDocClone.removeChild(ordLinesEle);
+				  ordLinesEle = inDocClone.getDocumentElement().getChildElement(XMLLiterals.ORDER_LINES,true);			  
+				  for(int i=0; i<ordLineList.size() ; i++) {
+					 XMLUtil.importNode(ordLinesEle,ordLineList.get(i));
+				  }
+				  System.out.println(ordLineList.toString());
+				  invokeYantraService(getProperty(ORDER_MONITOR), inDocClone);		
+			}
+		}
+
+	}
+
+}
--- indgutils/src/com/bridge/sterling/consts/XMLLiterals.java
+++ indgutils/src/com/bridge/sterling/consts/XMLLiterals.java
@@ -210,11 +210,6 @@ public static final String VALIDATE_ITEMS = "ValidateItems";
 public static final String RELEASE_NO = "ReleaseNo";
 public static final String GET_ORDER_DETAILS = "GetOrderDetails";
 public static final String SELLER_ORGANIZATION_CODE = "SellerOrganizationCode";
-public static final String MODIFICATION_REASON_CODE = "ModificationReasonCode";
-public static final String MODIFICATION_REASON_TEXT = "ModificationReasonText";
-public static final String CANCELLATION_TEXT = "CancellationText";
-
-
-
+public static final String SHIP_NODE="ShipNode";
 }
 

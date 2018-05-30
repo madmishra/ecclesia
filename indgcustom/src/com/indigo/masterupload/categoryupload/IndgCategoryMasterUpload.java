b65a3dba25809f625341a427307804990eab08bb
kajol.singh <kajol.singh@bridgesgi.com>
2018-05-30 17:48:00 +0530

    change6

--- indgcustom/src/com/indigo/masterupload/categoryupload/IndgCategoryMasterUpload.java
+++ indgcustom/src/com/indigo/masterupload/categoryupload/IndgCategoryMasterUpload.java
@@ -291,12 +291,10 @@ public class IndgCategoryMasterUpload extends AbstractCustomApi {
   
   public YFCDocument getItemListDocumentFromList(){
     YFCDocument itemListDoc  = null;
-    if(!XmlUtils.isVoid(itemListDoc)){
     	for(String itemID: itemIDList) {
     		System.out.println(itemIDList + "dskjhgdsg");
     		itemListDoc = YFCDocument.createDocument(XMLLiterals.ITEM_LIST);
     		itemListDoc.createElement(XMLLiterals.ITEM).setAttribute(XMLLiterals.ITEM_ID, itemID);
-    	}
     }
     System.out.println(itemListDoc + "itemListDoc");
     return itemListDoc;

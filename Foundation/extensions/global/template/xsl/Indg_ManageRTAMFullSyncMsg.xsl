ad1377cbd9a6a62b8d8cc15d248c968fbcfce381
Murali Pachiappan <murali.pachiappan@bridgesgi.com>
2018-05-31 14:03:50 +0530

    RTAM Code Changes

---
+++ Foundation/extensions/global/template/xsl/Indg_ManageRTAMFullSyncMsg.xsl
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+	<xsl:template match="/">
+		<xsl:variable name="itemID">
+		<xsl:value-of select="InventoryItem/@ItemID" />
+		</xsl:variable>
+		<AvailabilityChanges>
+			<xsl:for-each select="InventoryItem/AvailabilityChanges/AvailabilityChange">
+				<AvailabilityChange>
+					<xsl:attribute name="OnhandAvailableQuantity">
+						<xsl:value-of select="@OnhandAvailableQuantity" />
+					</xsl:attribute>
+					<xsl:attribute name="OnhandAvailableDate">
+						<xsl:value-of select="@AlertRaisedOn" />
+					</xsl:attribute>
+					<xsl:attribute name="Node">
+						<xsl:value-of select="@Node" />
+					</xsl:attribute>
+					<Item>
+						<xsl:attribute name="ItemID">
+							<xsl:value-of select="$itemID" />
+						</xsl:attribute>
+						<xsl:attribute name="OrganizationCode">Indigo_CA</xsl:attribute>
+					</Item>
+				</AvailabilityChange>
+			</xsl:for-each>
+			
+		</AvailabilityChanges>
+	</xsl:template>
+</xsl:stylesheet>
---
+++ indgcustom/src/com/indigo/inventory/IndgFullInventoryExport.java
@@ -0,0 +1,56 @@
+package com.indigo.inventory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import com.bridge.sterling.consts.ExceptionLiterals;
+import com.bridge.sterling.framework.api.AbstractCustomApi;
+import com.bridge.sterling.utils.ExceptionUtil;
+import com.yantra.yfc.dom.YFCDocument;
+
+/**
+ * This method writes to the file for export of
+ * inventory Dump
+ * 
+ * @author BSG109
+ *
+ */
+public class IndgFullInventoryExport extends AbstractCustomApi {
+
+  private static final String RTAM_FULL_UPLOAD_FILE_LOCATION = "RTAM_FULL_UPLOAD_FILE_LOCATION";
+  private static OutputStream fileOutputStream = null;
+  int inputMessageCount = 0;
+  @Override
+  public YFCDocument invoke(YFCDocument inXml) {
+      try {
+        String sInventoryUpload = inXml.toString();
+        if (sInventoryUpload.startsWith("<?xml ")) {
+          sInventoryUpload = sInventoryUpload.substring(sInventoryUpload.indexOf("?>") + 2);
+        }
+        setFileOutputStream();
+        fileOutputStream.write(sInventoryUpload.getBytes());
+        if(inputMessageCount == 25) 
+          fileOutputStream.flush();
+        inputMessageCount++;
+      } catch (Exception exp) {
+        throw ExceptionUtil.getYFSException(ExceptionLiterals.ERRORCODE_RTAM_UPLOAD, exp);
+      }
+    return inXml;
+  }
+  
+  
+  /**
+   * This method opens the steam in case of Stream if null
+   * 
+   * @return
+   * @throws FileNotFoundException
+   */
+  private void setFileOutputStream() throws FileNotFoundException {
+    
+    if(null == fileOutputStream) {
+      fileOutputStream = new FileOutputStream(new File(getProperty(RTAM_FULL_UPLOAD_FILE_LOCATION)),true);
+    }
+  }
+}

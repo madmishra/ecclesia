0f39476cdd40d010bb4ba36f77459a2e54bca809
Murali Pachiappan <murali.pachiappan@bridgesgi.com>
2018-06-06 21:11:48 +0530

    Customer Override Properties

---
+++ Foundation/script/startDeltaSyncServer_qa.sh
@@ -0,0 +1,2 @@
+cd /data/oms95/Foundation/bin
+nohup /data/oms95/Foundation/bin/startIntegrationServer.sh IndgDeltaSync \"-Xms512m -Xmx512m -XX:MaxPermSize=512m\" > /data/oms95/Foundation/logs/Delta_Sync.log &

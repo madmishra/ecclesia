37e15bed88e717503df240c3c1aa377353f2fb60
kavya.s <kavya.s@bridgesgi.com>
2018-06-19 17:03:58 +0530

    ue

--- indgcustom/src/com/indigo/om/outbound/api/BeforeChangeOrderUserExit.java
+++ indgcustom/src/com/indigo/om/outbound/api/BeforeChangeOrderUserExit.java
@@ -38,8 +38,8 @@ public class BeforeChangeOrderUserExit extends AbstractCustomApi {
 			System.out.println("zbyvu6ywn7"+orderLine.getAttribute(XMLLiterals.ACTION));
 			System.out.println("nvbmyoiiiiiiihgtys"+orderLine.getAttribute(XMLLiterals.MODIFICATION_REFRENCE_1));
 			
-			if((orderLine.getAttribute(XMLLiterals.ACTION).
-					equals(CANCEL)) && XmlUtils.isVoid(orderLine.getAttribute(XMLLiterals.MODIFICATION_REFRENCE_1))) {
+			if((!XmlUtils.isVoid(orderLine.getAttribute(XMLLiterals.ACTION))&& (orderLine.getAttribute(XMLLiterals.ACTION).
+					equals(CANCEL))) && XmlUtils.isVoid(orderLine.getAttribute(XMLLiterals.MODIFICATION_REFRENCE_1))) {
 		System.out.println("hfdjdkfl"+orderLine);
 				orderLine.setAttribute(XMLLiterals.MODIFICATION_REFRENCE_1, MANUAL);
 				System.out.println("MANUAL ATTRIBUTE"+inXml);

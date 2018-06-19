0ddd5eb7b3140d7eef2fb0d3588893e4e973450b
kavya.s <kavya.s@bridgesgi.com>
2018-06-19 17:15:53 +0530

    ue

--- indgcustom/src/com/indigo/om/outbound/api/BeforeChangeOrderUserExit.java
+++ indgcustom/src/com/indigo/om/outbound/api/BeforeChangeOrderUserExit.java
@@ -37,9 +37,12 @@ public class BeforeChangeOrderUserExit extends AbstractCustomApi {
 			System.out.println("**********"+orderLine);
 			System.out.println("zbyvu6ywn7"+orderLine.getAttribute(XMLLiterals.ACTION));
 			System.out.println("nvbmyoiiiiiiihgtys"+orderLine.getAttribute(XMLLiterals.MODIFICATION_REFRENCE_1));
-			
-			if((!XmlUtils.isVoid(orderLine.getAttribute(XMLLiterals.ACTION))&& (orderLine.getAttribute(XMLLiterals.ACTION).
-					equals(CANCEL))) && XmlUtils.isVoid(orderLine.getAttribute(XMLLiterals.MODIFICATION_REFRENCE_1))) {
+			System.out.println("mkMNFkdnknkGf"+!XmlUtils.isVoid(orderLine.getAttribute(XMLLiterals.ACTION)));
+			System.out.println("njkdfnkjZNGKsjrkhjnazijh"+orderLine.getAttribute(XMLLiterals.ACTION).
+					equals(CANCEL));
+			System.out.println("jkajfkjkjfkojf"+XmlUtils.isVoid(orderLine.getAttribute(XMLLiterals.MODIFICATION_REFRENCE_1)));
+			if(!XmlUtils.isVoid(orderLine.getAttribute(XMLLiterals.ACTION))&& (orderLine.getAttribute(XMLLiterals.ACTION).
+					equals(CANCEL)) && XmlUtils.isVoid(orderLine.getAttribute(XMLLiterals.MODIFICATION_REFRENCE_1))) {
 		System.out.println("hfdjdkfl"+orderLine);
 				orderLine.setAttribute(XMLLiterals.MODIFICATION_REFRENCE_1, MANUAL);
 				System.out.println("MANUAL ATTRIBUTE"+inXml);

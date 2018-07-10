b7e0f2d3e3d823aee94da59f8c8c714fd2128e6b
kajol.singh <kajol.singh@bridgesgi.com>
2018-07-10 15:04:33 +0530

    changes

--- indgcustom/src/com/indigo/om/outbound/api/IndgAbandonmentTimeSAP062.java
+++ indgcustom/src/com/indigo/om/outbound/api/IndgAbandonmentTimeSAP062.java
@@ -51,7 +51,7 @@ public class IndgAbandonmentTimeSAP062 extends AbstractCustomApi {
 				adjustQuantityofInventory(inpQuantity, primeLineNo, docAdjustInv, inXml);
 			}
 		}
-		System.out.println(docAdjustInv);
+		System.out.println(docAdjustInv + "aaaaaaaaa");
 		invokeYantraApi(XMLLiterals.ADJUST_INVENTORY_API, docAdjustInv);
 	}
 	

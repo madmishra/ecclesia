2071d99216d79bbfc11396d99670d4433532f41b
kavya.s <kavya.s@bridgesgi.com>
2018-07-23 20:23:10 +0530

    changes

---
+++ indgcustom/src/com/indigo/masterupload/itemfeedupload/IndgCreateCategoryItem.java
@@ -0,0 +1,98 @@
+package com.indigo.masterupload.itemfeedupload;
+
+import com.bridge.sterling.consts.XMLLiterals;
+import com.bridge.sterling.framework.api.AbstractCustomApi;
+import com.bridge.sterling.utils.XPathUtil;
+import com.indigo.masterupload.categoryupload.IndgCategoryMasterUpload;
+import com.sterlingcommerce.tools.datavalidator.XmlUtils;
+import com.yantra.yfc.dom.YFCDocument;
+import com.yantra.yfc.dom.YFCElement;
+
+/**
+ * 
+ * @author BSG109
+ *
+ */
+
+public class IndgCreateCategoryItem extends AbstractCustomApi {
+	
+	  String organizationCode="Indigo_CA"; 
+	  private static final String CREATE_ACTION = "Create";
+	  private static final String DEFAULT_CATEGORY_PATH = "DefaultCategoryPath";
+	  private static final String EMPTY_STRING = "";
+	  private static final String CATEGORY_ALERT_FLOW="CATEGORY_ALERT_FLOW";
+	
+
+	  /**
+	   * This is the starting point of the class
+	   * 
+	   */
+	  
+	  @Override
+	  public YFCDocument invoke(YFCDocument inXml) {
+		  createCategoryItem(inXml);
+		  return inXml;
+	  }
+	  
+	  /**
+	   * This method calls modifyCategoryItem for creating categoryFeed
+	   * 
+	   * @param inXml
+	   */
+	  
+	  private void createCategoryItem(YFCDocument inXml) {
+	    YFCElement itemEle = inXml.getDocumentElement().getChildElement(XMLLiterals.ITEM);
+	    String categoryId = IndgManageItemFeed.getCategoryID(itemEle);
+	    if(!XmlUtils.isVoid(categoryId)) {
+	      YFCDocument categoryList = getCategoryList(categoryId, organizationCode);
+	      if(categoryList.getDocumentElement().hasChildNodes()) {
+	        String categoryPath = XPathUtil.getXpathAttribute(categoryList, 
+	          "/CategoryList/Category/@CategoryPath");
+	        invokeYantraApi(XMLLiterals.MODIFY_CATEGORY_ITEM, 
+	          IndgManageItemFeed.getInputDocForModifyCategoryItem(itemEle.getAttribute(XMLLiterals.ITEM_ID)
+	              ,CREATE_ACTION,categoryPath,organizationCode));
+	        } else {
+	        invokeYantraService(getProperty(CATEGORY_ALERT_FLOW), inXml);
+	      }
+	    } else {
+	      invokeYantraApi(XMLLiterals.MODIFY_CATEGORY_ITEM, 
+	          IndgManageItemFeed.getInputDocForModifyCategoryItem(itemEle.getAttribute(XMLLiterals.ITEM_ID)
+	              ,CREATE_ACTION,getProperty(DEFAULT_CATEGORY_PATH),organizationCode));
+	    }
+	    setItemType(itemEle);
+	  }
+	  
+	  /**
+	   * This method calls getCategoryList API
+	   * @param categoryId
+	   * @param org
+	   * @return
+	   */
+	  
+	   private YFCDocument getCategoryList(String categoryId, String org) {
+	     return invokeYantraApi(XMLLiterals.GET_CATEGORY_LIST, 
+	         IndgCategoryMasterUpload.getInputXmlForGetCategoryList(categoryId,org,EMPTY_STRING),
+	           IndgCategoryMasterUpload.formTemplateXmlForgetCategoryList());
+	   }
+	   
+	   /**
+	    * 
+	    * This method sets ItemType from commodity code attribute
+	    * from the item element. 
+	    * 
+	    * @param itemEle
+	    */
+	   
+	   public static void setItemType(YFCElement itemEle) {
+	     if(!XmlUtils.isVoid(itemEle.getChildElement(XMLLiterals.CLASSIFICATION_CODES))) {
+	       String categoryID = itemEle.getChildElement(XMLLiterals.CLASSIFICATION_CODES)
+	           .getAttribute(XMLLiterals.COMMODITY_CODE,EMPTY_STRING);
+	       if(!XmlUtils.isVoid(categoryID)) {
+	         itemEle.getChildElement(XMLLiterals.PRIMARY_INFORMATION)
+	         .setAttribute(XMLLiterals.ITEM_TYPE,categoryID);
+	       }
+	     }
+	   }
+	  
+	  
+}

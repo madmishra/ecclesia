7f96f77bab3d3f0c2d7ad16e0bec90a52356ce3b
Neelkant <neelkant.jain@bridgesgi.com>
2018-07-27 11:19:10 +0530

    hiding search details

--- Foundation/extensions/wsc/webpages/mobile/home/search/SearchExtnUI.js
+++ Foundation/extensions/wsc/webpages/mobile/home/search/SearchExtnUI.js
@@ -1,5 +1,5 @@
 
-scDefine(["dojo/text!./templates/SearchExtn.html","scbase/loader!dijit/form/Button","scbase/loader!dojo/_base/declare","scbase/loader!dojo/_base/kernel","scbase/loader!dojo/_base/lang","scbase/loader!dojo/text","scbase/loader!sc/plat","scbase/loader!sc/plat/dojo/binding/ButtonDataBinder","scbase/loader!sc/plat/dojo/utils/BaseUtils"]
+scDefine(["dojo/text!./templates/SearchExtn.html","scbase/loader!dijit/form/Button","scbase/loader!dojo/_base/declare","scbase/loader!dojo/_base/kernel","scbase/loader!dojo/_base/lang","scbase/loader!dojo/text","scbase/loader!idx/form/FilteringSelect","scbase/loader!idx/form/TextBox","scbase/loader!sc/plat","scbase/loader!sc/plat/dojo/binding/ButtonDataBinder","scbase/loader!sc/plat/dojo/binding/ComboDataBinder","scbase/loader!sc/plat/dojo/binding/SimpleDataBinder","scbase/loader!sc/plat/dojo/utils/BaseUtils"]
  , function(			 
 			    templateText
 			 ,
@@ -13,9 +13,17 @@ scDefine(["dojo/text!./templates/SearchExtn.html","scbase/loader!dijit/form/Butt
 			 ,
 			    _dojotext
 			 ,
+			    _idxFilteringSelect
+			 ,
+			    _idxTextBox
+			 ,
 			    _scplat
 			 ,
 			    _scButtonDataBinder
+			 ,
+			    _scComboDataBinder
+			 ,
+			    _scSimpleDataBinder
 			 ,
 			    _scBaseUtils
 ){
